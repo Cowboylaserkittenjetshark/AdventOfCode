f2b2f76f3a0ae2002d12cda59353df8a3c16e228
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-12-02 03:00:01 -0500

    feat: day 2 part 1 solution

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -1,12 +1,64 @@
 use clap::Parser;
-use std::{fs::read_to_string, path::PathBuf, process::exit};
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+    process::exit,
+};
 
 fn main() {
-    let file = Args::parse().input;
-    let input = read_to_string(file).unwrap_or_else(|e| {
-        eprintln!("Error: {e}");
-        exit(1)
-    });
+    let input = BufReader::new(File::open(Args::parse().input).unwrap_or_else(|err| die(err)));
+    let mut safe_count = 0;
+
+    'report_layer: for line in input.lines() {
+        if let Ok(report) = line {
+            let levels = report
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap_or_else(|e| die(e)));
+            let mut orientation: LevelOrientation = LevelOrientation::None;
+            for (a, b) in levels.tuple_windows() {
+                let local_orientation;
+
+                if a < b {
+                    local_orientation = LevelOrientation::Inc;
+                } else if a > b {
+                    local_orientation = LevelOrientation::Dec;
+                } else {
+                    continue 'report_layer;
+                }
+
+                if let LevelOrientation::None = orientation {
+                    orientation = local_orientation;
+                } else {
+                    if local_orientation != orientation {
+                        continue 'report_layer;
+                    }
+                }
+
+                let diff = a.abs_diff(b);
+                if diff < 1 || diff > 3 {
+                    continue 'report_layer;
+                }
+            }
+        } else {
+            die("Invalid input file");
+        }
+        safe_count += 1;
+    }
+    println!("Safe reports: {safe_count}");
+}
+
+#[derive(PartialEq, Eq)]
+enum LevelOrientation {
+    None,
+    Inc,
+    Dec,
+}
+
+fn die<T: ToString>(err: T) -> ! {
+    eprintln!("Error: {}", err.to_string());
+    exit(1);
 }
 
 #[derive(Parser, Debug)]
