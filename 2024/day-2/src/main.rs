d6cbcb09a10c8b8766037e5f3fb01e55c9c582ef
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-12-03 15:07:07 -0500

    feat: add day 2 solution

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -1,13 +1,19 @@
 use clap::Parser;
-use itertools::Itertools;
+use itertools::{chain, Itertools};
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    iter,
     path::PathBuf,
     process::exit,
 };
 
 fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
     let input = BufReader::new(File::open(Args::parse().input).unwrap_or_else(|err| die(err)));
     let mut safe_count = 0;
 
@@ -49,7 +55,42 @@ fn main() {
     println!("Safe reports: {safe_count}");
 }
 
-#[derive(PartialEq, Eq)]
+fn part_2() {
+    let input = BufReader::new(File::open(Args::parse().input).unwrap_or_else(|err| die(err)));
+    let mut safe_count = 0;
+
+    for line in input.lines() {
+        if let Ok(report) = line {
+            let levels: Vec<i32> = report
+                .split_whitespace()
+                .map(|s| s.parse().unwrap_or_else(|e| die(e)))
+                .collect();
+            safe_count += chain!(
+                iter::once(chain!(&levels[..], &levels[0..0])),
+                (0..levels.len()).map(|i| chain!(&levels[0..i], &levels[i + 1..]))
+            )
+            .any(|r| {
+                let mut orientation = 0;
+                r.tuple_windows().all(|(a, b)| {
+                    let diff = b - a;
+                    let lo = diff.signum();
+                    let diff = diff.abs();
+                    if diff < 1 || diff > 3 || lo == 0 || (lo != orientation && orientation != 0) {
+                        false
+                    } else {
+                        orientation = lo;
+                        true
+                    }
+                })
+            }) as i32;
+        } else {
+            die("Invalid input file");
+        }
+    }
+    println!("Safe reports (damped): {safe_count}");
+}
+
+#[derive(PartialEq, Eq, Clone)]
 enum LevelOrientation {
     None,
     Inc,
