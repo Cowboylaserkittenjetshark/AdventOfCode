b0a6d7f284b4ac8e80b30cb75116dc0efcce4e14
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-12-17 16:13:12 -0500

    feat: add day 7 part 1

---
+++ 2024/day-7/src/main.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+use itertools::{repeat_n, Itertools};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    ops::AddAssign,
+    path::PathBuf,
+    process::exit,
+};
+
+fn main() {
+    let input = BufReader::new(File::open(Args::parse().input).unwrap_or_else(|err| die(err)));
+    let mut total_calibration_result = 0;
+    'line: for line in input.lines().map(|res| res.unwrap_or_else(|e| die(e))) {
+        let mut tokens = line.split_whitespace();
+        let test_value = tokens.next().unwrap_or_else(|| die("Invalid input file"));
+        let test_value = test_value[..test_value.len() - 1].parse().unwrap();
+        let tokens = tokens.map(|s| s.parse::<u64>().unwrap_or_else(|e| die(e)));
+        let nums = tokens.collect_vec();
+        let op_perms = repeat_n(['+', '*'], nums.len() - 1).multi_cartesian_product();
+        for perm in op_perms {
+            let mut nums = nums.iter();
+            let ops = perm.iter();
+            let a = nums.next().unwrap_or_else(|| die("Invalid input file"));
+            let res_value = do_op(*a, nums, ops);
+            if res_value == test_value {
+                total_calibration_result += test_value;
+                continue 'line;
+            }
+        }
+    }
+    println!("Total calibration result: {total_calibration_result}");
+}
+
+fn do_op(a: u64, mut nums: std::slice::Iter<'_, u64>, mut ops: std::slice::Iter<'_, char>) -> u64 {
+    if let Some(b) = nums.next() {
+        match ops
+            .next()
+            .expect("Operator permutation had an unexpected length")
+        {
+            '+' => do_op(a + b, nums, ops),
+            '*' => do_op(a * b, nums, ops),
+            _ => unreachable!("ops will only contain + and *"),
+        }
+    } else {
+        return a;
+    }
+}
+
+fn die<T: ToString>(err: T) -> ! {
+    eprintln!("Error: {}", err.to_string());
+    exit(1);
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to the input file
+    input: PathBuf,
+}
